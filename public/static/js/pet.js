0b902f2a8252a2289a60186a5f419b7d765199cc
hanhnt12 <nth.hanhnt@gmail.com>
2017-11-15 22:02:26 +0700

    update layout

--- public/static/js/pet.js
+++ public/static/js/pet.js
@@ -1,5 +1,3 @@
 $(document).ready(function(){
   $('[data-toggle="tooltip"]').tooltip(); 
-  $('.carousel-indicators li').first().addClass('active');
-  $('.carousel-item').first().addClass('active');
 });

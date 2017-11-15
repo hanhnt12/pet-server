cf257bb179ecbe0ca54c3736f2ffae85efcb7b8f
hanhnt12 <nth.hanhnt@gmail.com>
2017-11-15 19:27:32 +0700

    update not found page

--- public/static/js/pet.js
+++ public/static/js/pet.js
@@ -1,3 +1,5 @@
 $(document).ready(function(){
   $('[data-toggle="tooltip"]').tooltip(); 
+  $('.carousel-indicators li').first().addClass('active');
+  $('.carousel-item').first().addClass('active');
 });

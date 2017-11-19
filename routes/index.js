a28fc2ab421e7b18c805656159ab1bbca9970f7e
hanhnt12 <nth.hanhnt@gmail.com>
2017-11-19 20:18:36 +0700

    update layout

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,8 @@ var express = require('express');
 var router = express.Router();
 let Product = require('../models/ProductModel');
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  Product.find({}, (err, docs) => {
-    res.render('index', { products: docs });
-  });
+router.get('/', function (req, res, next) {
+  res.render('index');
 });
 
 module.exports = router;
